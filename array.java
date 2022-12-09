3b5d2fc0a06370b4d827c1293905cbaaf6c133db
geekguri-tech <geekguri1232@gmail.com>
2022-12-09 13:55:01 +0530

    Changed few things

---
+++ array.java
@@ -0,0 +1,12 @@
+public class array {
+    public static void main(String args[])
+    {
+        int arr[] = {1, 5, 3};
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++)
+        {
+            sum = sum+arr[i];
+        }
+        System.out.println(sum);
+}
+}
