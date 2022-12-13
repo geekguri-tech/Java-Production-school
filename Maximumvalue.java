19044507725e41fac010089595ff2e3426dd3566
geekguri-tech <geekguri1232@gmail.com>
2022-12-13 16:08:06 +0530

    Program for finding max, min, secmin values in an Array

--- Maximumvalue.java
+++ Maximumvalue.java
@@ -1,17 +1,71 @@
+import java.util.*;
+
 public class Maximumvalue 
 {
- public static void main(String args[])
- {
-    int arr[] = {6,8,9,11,89,100,56,34,7,90,00,65,12};
-    int ans = 0;
 
-    for(int i = 0; i < arr.length; i++ )
+    public static void main(String args[]) 
     {
-        if(arr[i] > ans)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements you want in Array");
+        int h = sc.nextInt();
+
+        int arra[] = new int[h];
+        for (int i = 0; i < h; i++) 
         {
-            ans = arr[i];
+            arra[i] = sc.nextInt();
         }
+        sc.close();
+    
+        System.out.println("Maximum Element of the Array is " + liquid(arra));
+        System.out.println("Minimum Element in the array is " + minimumvalue.liquid2(arra));
+        System.out.println("Second Maximum value is " + secondmaximum.liquid3(arra));
     }
-    System.out.println(ans);
- }   
+
+    static int liquid (int arr[]) 
+    {
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] > ans) 
+            {
+                ans = arr[i];
+            }
+        }
+        return ans;
+    }
+}
+
+class minimumvalue extends Maximumvalue
+{
+    static int liquid2(int arr2[])
+    {
+        int ans2 = arr2[0];
+        for (int ii = 0; ii < arr2.length; ii++)
+        {
+            if (arr2[ii] < ans2) 
+            {
+                ans2 = arr2[ii];
+            }
+        }
+        return ans2;
+    }
+}
+
+class secondmaximum extends Maximumvalue
+{
+    static int liquid3(int arr3[])
+    {
+        int mx = liquid(arr3);
+        int n = arr3.length;
+
+        for(int iii = 0; iii < n; iii++)
+        {
+            if(arr3[iii] == mx)
+            {
+                arr3[iii] = Integer.MIN_VALUE;
+            }
+        }
+        int ans4 = liquid(arr3);
+        return ans4;
+    }
 }
