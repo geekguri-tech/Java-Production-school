a5d96e0b9c947248d19083b4a6243f14cda14b1c
geekguri-tech <geekguri1232@gmail.com>
2022-12-15 19:17:42 +0530

    Added mathematical way to swap integers

--- Swapvalues.java
+++ Swapvalues.java
@@ -14,5 +14,15 @@ public class Swapvalues
         System.out.println("Values after Swap: ");
         System.out.print(a + ", ");
         System.out.println(b);
+
+        int aa = 9; // Using sum and Difference method
+        int bb = 3;
+        aa = aa+bb; // a = 12
+        bb = aa-bb; // b = 9
+        aa = aa-bb; // a = 3
+        System.out.println("Values after using Mathematical way of Swap: ");
+        System.out.print(aa + ", ");
+        System.out.println(bb);
+
     }    
 }
